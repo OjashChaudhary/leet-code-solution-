abe6ee7cd85970c39316d034b4038eb237e59ce3
OjashChaudhary <ojashchaudhary247@gmail.com>
2026-08-04 14:55:44 +0530

    Time: 28 ms (95.87%), Space: 97.8 MB (98.22%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,33 @@
+class MinStack {
+    private int[] stack;
+    private int[] min;
+    private int top;
+
+    public MinStack() {
+        stack = new int[30000];
+        min = new int[30000];
+        top = -1;
+    }
+
+    public void push(int val) {
+        top++;
+        stack[top] = val;
+        if (top == 0) {
+            min[top] = val;
+        } else {
+            min[top] = Math.min(val, min[top - 1]);
+        }
+    }
+
+    public void pop() {
+        top--;
+    }
+
+    public int top() {
+        return stack[top];
+    }
+
+    public int getMin() {
+        return min[top];
+    }
+}
