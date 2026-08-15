732fd0aa9ced51e8f9f00b7d0b33d819800826f5
OjashChaudhary <ojashchaudhary247@gmail.com>
2026-08-15 23:33:09 +0530

    Time: 7 ms (96.96%), Space: 64.7 MB (96.8%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,34 +1,34 @@
 class Solution {
     public List<List<String>> partition(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        
+        for (int end = 0; end < n; end++) {
+            for (int start = 0; start <= end; start++) {
+                if (s.charAt(start) == s.charAt(end) && (end - start <= 2 || dp[start + 1][end - 1])) {
+                    dp[start][end] = true;
+                }
+            }
+        }
+        
         List<List<String>> result = new ArrayList<>();
         List<String> current = new ArrayList<>();
-        backtrack(s, 0, current, result);
+        backtrack(s, 0, current, result, dp);
         return result;
     }
     
-    private void backtrack(String s, int start, List<String> current, List<List<String>> result) {
+    private void backtrack(String s, int start, List<String> current, List<List<String>> result, boolean[][] dp) {
         if (start == s.length()) {
             result.add(new ArrayList<>(current));
             return;
         }
         
         for (int end = start; end < s.length(); end++) {
-            if (isPalindrome(s, start, end)) {
+            if (dp[start][end]) {
                 current.add(s.substring(start, end + 1));
-                backtrack(s, end + 1, current, result);
+                backtrack(s, end + 1, current, result, dp);
                 current.remove(current.size() - 1);
             }
         }
-    }
-    
-    private boolean isPalindrome(String s, int left, int right) {
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
     }
 }
