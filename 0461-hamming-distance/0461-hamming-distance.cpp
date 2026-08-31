e8efeb9103699d43744fbfd236518e12a1f8dd85
OjashChaudhary <ojashchaudhary247@gmail.com>
2026-08-31 22:12:42 +0530

    Time: 0 ms (100%), Space: 8 MB (35.64%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int xorResult = x ^ y;
+        int count = 0;
+        
+        while (xorResult != 0) {
+            xorResult = xorResult & (xorResult - 1);
+            count++;
+        }
+        
+        return count;
+    }
+};
