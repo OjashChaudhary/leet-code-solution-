1b469cf221e8dbac462a6e2cddb077f69c68fedc
OjashChaudhary <ojashchaudhary247@gmail.com>
2026-08-31 22:42:28 +0530

    Time: 30 ms (86.61%), Space: 56.2 MB (83.89%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.java
+++ 0355-design-twitter/0355-design-twitter.java
@@ -1,69 +1,63 @@
-import java.util.*;
-
 class Twitter {
-
-    class Tweet {
-        int id, time;
-        Tweet next;
-
-        Tweet(int id, int time) {
-            this.id = id;
-            this.time = time;
-        }
-    }
-
-    HashMap<Integer, HashSet<Integer>> follow = new HashMap<>();
-    HashMap<Integer, Tweet> tweets = new HashMap<>();
-    int time = 0;
-
+    private Map<Integer, Set<Integer>> followMap;
+    private Map<Integer, List<int[]>> tweetMap;
+    private int timestamp;
+    
     public Twitter() {
+        followMap = new HashMap<>();
+        tweetMap = new HashMap<>();
+        timestamp = 0;
     }
-
+    
     public void postTweet(int userId, int tweetId) {
-        Tweet t = new Tweet(tweetId, time++);
-        t.next = tweets.get(userId);
-        tweets.put(userId, t);
+        tweetMap.putIfAbsent(userId, new ArrayList<>());
+        tweetMap.get(userId).add(new int[]{timestamp, tweetId});
+        timestamp++;
     }
-
+    
     public List<Integer> getNewsFeed(int userId) {
-        PriorityQueue<Tweet> pq = new PriorityQueue<>(
-            (a, b) -> b.time - a.time
-        );
-
-        if (tweets.containsKey(userId)) {
-            pq.add(tweets.get(userId));
-        }
-
-        if (follow.containsKey(userId)) {
-            for (int id : follow.get(userId)) {
-                if (tweets.containsKey(id)) {
-                    pq.add(tweets.get(id));
-                }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        
+        Set<Integer> followees = followMap.getOrDefault(userId, new HashSet<>());
+        followees.add(userId);
+        
+        for (int followeeId : followees) {
+            List<int[]> tweets = tweetMap.get(followeeId);
+            if (tweets == null) {
+                continue;
+            }
+            
+            int n = tweets.size();
+            int start = Math.max(0, n - 10);
+            
+            for (int i = start; i < n; i++) {
+                pq.offer(tweets.get(i));
             }
         }
-
-        List<Integer> ans = new ArrayList<>();
-
-        while (!pq.isEmpty() && ans.size() < 10) {
-            Tweet t = pq.poll();
-            ans.add(t.id);
-
-            if (t.next != null) {
-                pq.add(t.next);
-            }
+        
+        List<Integer> result = new ArrayList<>();
+        List<int[]> allTweets = new ArrayList<>();
+        
+        while (!pq.isEmpty()) {
+            allTweets.add(pq.poll());
         }
-
-        return ans;
+        
+        int limit = Math.min(10, allTweets.size());
+        for (int i = 0; i < limit; i++) {
+            result.add(allTweets.get(i)[1]);
+        }
+        
+        return result;
     }
-
+    
     public void follow(int followerId, int followeeId) {
-        follow.putIfAbsent(followerId, new HashSet<>());
-        follow.get(followerId).add(followeeId);
+        followMap.putIfAbsent(followerId, new HashSet<>());
+        followMap.get(followerId).add(followeeId);
     }
-
+    
     public void unfollow(int followerId, int followeeId) {
-        if (follow.containsKey(followerId)) {
-            follow.get(followerId).remove(followeeId);
+        if (followMap.containsKey(followerId)) {
+            followMap.get(followerId).remove(followeeId);
         }
     }
 }
