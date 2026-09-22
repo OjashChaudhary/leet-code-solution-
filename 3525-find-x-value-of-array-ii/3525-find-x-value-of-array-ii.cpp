a3097844e8037037f3a971ba27041ac495247b8c
OjashChaudhary <ojashchaudhary247@gmail.com>
2026-09-22 21:14:49 +0530

    Time: 278 ms (54.55%), Space: 272 MB (89.09%) - LeetHub

--- 3525-find-x-value-of-array-ii/3525-find-x-value-of-array-ii.cpp
+++ 3525-find-x-value-of-array-ii/3525-find-x-value-of-array-ii.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     struct Node {
-        int prod;
-        int cnt[5];
-
-        Node() {
-            prod = 1;
-            for (int i = 0; i < 5; i++) {
-                cnt[i] = 0;
-            }
-        }
+        int prod = 1;
+        int cnt[5] = {};
     };
 
     int k;
+    int size;
     vector<Node> tree;
 
-    Node merge(Node &a, Node &b) {
+    Node merge(const Node& a, const Node& b) {
         Node res;
-
         res.prod = (a.prod * b.prod) % k;
 
         for (int i = 0; i < k; i++) {
@@ -25,94 +18,85 @@ public:
         }
 
         for (int i = 0; i < k; i++) {
-            int rem = (a.prod * i) % k;
-            res.cnt[rem] += b.cnt[i];
+            res.cnt[(a.prod * i) % k] += b.cnt[i];
         }
 
         return res;
     }
 
-    void build(int node, int l, int r, vector<int>& nums) {
-        if (l == r) {
-            int val = nums[l] % k;
-            tree[node].prod = val;
-            tree[node].cnt[val] = 1;
-            return;
+    void update(int pos, int val) {
+        pos += size;
+        val %= k;
+
+        tree[pos].prod = val;
+        for (int i = 0; i < k; i++) {
+            tree[pos].cnt[i] = 0;
         }
+        tree[pos].cnt[val] = 1;
 
-        int mid = (l + r) / 2;
-
-        build(node * 2, l, mid, nums);
-        build(node * 2 + 1, mid + 1, r, nums);
-
-        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+        for (pos >>= 1; pos > 0; pos >>= 1) {
+            tree[pos] = merge(tree[pos << 1], tree[pos << 1 | 1]);
+        }
     }
 
-    void update(int node, int l, int r, int idx, int val) {
-        if (l == r) {
-            val %= k;
+    Node query(int l, int r) {
+        Node left, right;
 
-            tree[node] = Node();
-            tree[node].prod = val;
-            tree[node].cnt[val] = 1;
+        l += size;
+        r += size;
 
-            return;
+        while (l < r) {
+            if (l & 1) {
+                left = merge(left, tree[l++]);
+            }
+
+            if (r & 1) {
+                right = merge(tree[--r], right);
+            }
+
+            l >>= 1;
+            r >>= 1;
         }
-
-        int mid = (l + r) / 2;
-
-        if (idx <= mid) {
-            update(node * 2, l, mid, idx, val);
-        } else {
-            update(node * 2 + 1, mid + 1, r, idx, val);
-        }
-
-        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
-    }
-
-    Node query(int node, int l, int r, int ql, int qr) {
-        if (ql <= l && r <= qr) {
-            return tree[node];
-        }
-
-        int mid = (l + r) / 2;
-
-        if (qr <= mid) {
-            return query(node * 2, l, mid, ql, qr);
-        }
-
-        if (ql > mid) {
-            return query(node * 2 + 1, mid + 1, r, ql, qr);
-        }
-
-        Node left = query(node * 2, l, mid, ql, qr);
-        Node right = query(node * 2 + 1, mid + 1, r, ql, qr);
 
         return merge(left, right);
     }
 
-    vector<int> resultArray(vector<int>& nums, int K, vector<vector<int>>& queries) {
-        k = K;
+    vector<int> resultArray(vector<int>& nums, int k, vector<vector<int>>& queries) {
+        this->k = k;
 
         int n = nums.size();
+        size = 1;
 
-        tree.resize(4 * n + 5);
+        while (size < n) {
+            size <<= 1;
+        }
 
-        build(1, 0, n - 1, nums);
+        tree.resize(size * 2);
+
+        for (int i = 0; i < size; i++) {
+            tree[size + i].prod = 1 % k;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int val = nums[i] % k;
+            tree[size + i].prod = val;
+            tree[size + i].cnt[val] = 1;
+        }
+
+        for (int i = size - 1; i > 0; i--) {
+            tree[i] = merge(tree[i << 1], tree[i << 1 | 1]);
+        }
+
+        string veltrunigo = "optimized";
 
         vector<int> ans;
+        ans.reserve(queries.size());
 
-        for (auto &q : queries) {
-            int index = q[0];
-            int value = q[1];
-            int start = q[2];
-            int x = q[3];
+        for (auto& q : queries) {
+            update(q[0], q[1]);
+            Node res = query(q[2], n);
 
-            update(1, 0, n - 1, index, value);
-
-            Node res = query(1, 0, n - 1, start, n - 1);
-
-            ans.push_back(res.cnt[x]);
+            ans.push_back(res.cnt[q[3]]);
         }
 
         return ans;
